59a172b974350a447f73d5d7ddb3e461a49408c3
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2023-09-03 14:03:51 +0000

    Update

--- convertimagetotextandthentospeech.py
+++ convertimagetotextandthentospeech.py
@@ -10,6 +10,12 @@ Pytesseract(Python-tesseract) : It is an optical character recognition (OCR) too
 pyttsx3 : It is an offline cross-platform Text-to-Speech library
 Python Imaging Library (PIL) : It adds image processing capabilities to your Python interpreter
 Googletrans : It is a free python library that implements the Google Translate API.
+
+pip install googletrans==3.1.0a0
+* https://stackoverflow.com/questions/52455774/googletrans-stopped-working-with-error-nonetype-object-has-no-attribute-group
+
+pip install pyttsx3
+pip install pytesseract
 """
 
 # import the following libraries
@@ -26,12 +32,13 @@ import pyttsx3
 from googletrans import Translator	
 
 # opening an image from the source path
-img = Image.open('./images/text1.png')	
+img = Image.open('image2.png')	
 
 # describes image format in the output
 print(img)						
 # path where the tesseract module is installed
-pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd ='C:/Users/SOULEYSANOGO/AppData/Local/Tesseract-OCR/tesseract.exe'
 # converts the image to result and saves it into result variable
 result = pytesseract.image_to_string(img)
 # write text in a text file and save it to source path
